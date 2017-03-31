33191df46ca2b53cd7c691afac356f796b930e56
Nicolas Wagner <wagner.nicolas1@gmail.com>
2017-03-31 11:55:44 +0200

    fix migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,12 @@
-var Arbitrable = artifacts.require("./Arbitrated/Arbitrable.sol");
-var ArbitratedBuy = artifacts.require("./Tokens/ArbitratedBuy.sol");
-var ArbitratedBuy = artifacts.require("./Tokens/ArbitratedBuyDeposit.sol");
+var ArbitratedBuy = artifacts.require("./Arbitrated/ArbitratedBuy.sol");
 
-var Token = artifacts.require("./Tokens/Token.sol");
-
-var BuyableCourt = artifacts.require("./BuyableCourt.sol");
 var Court = artifacts.require("./Court.sol");
 //var Web3 = require('../node_modules/web3');
 
 module.exports = function(deployer) {
 	//var web3RPC = new Web3(deployer.provider);
-	deployer.deploy(Token);
+	deployer.deploy(Court);
+	deployer.deploy(ArbitratedBuy);
 
 	/*web3RPC.eth.getAccounts((error, accounts) => {
   		deployer.deploy(Kleroterion,[accounts[0],accounts[1],accounts[2]]);
