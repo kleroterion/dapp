c47fe712c7ca602fef8f3a88a1fca335f291f284
Nicolas Wagner <contact@wagner-nicolas.com>
2017-04-16 14:57:55 +0200

    fix deploy contract

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -5,8 +5,7 @@ contract Migrations {
   uint public last_completed_migration;
 
   modifier restricted() {
-    if (msg.sender != owner) throw;
-    _;
+    if (msg.sender == owner) _;
   }
 
   function Migrations() {
